10539b80e3bd3f757ab79f350572d689e667cc23
toniblom <toniblom91@gmail.com>
2023-09-04 20:48:26 +0300

    Additions to task 3

--- friendList/src/main/java/s23back/friendList/domain/Friend.java
+++ friendList/src/main/java/s23back/friendList/domain/Friend.java
@@ -11,14 +11,21 @@ public class Friend {
 		this.name = name;
 	}
 
+	@Override
+	public String toString() {
+		return "Friend [name=" + name + "]";
+	}
+
 	public Friend(String name) {
 		super();
 		this.name = name;
 	}
 
-	@Override
-	public String toString() {
-		return "Friend [name=" + name + "]";
+	public Friend() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
+	
+	
 	
 }
--- friendList/src/main/java/s23back/friendList/web/FriendListController.java
+++ friendList/src/main/java/s23back/friendList/web/FriendListController.java
@@ -1,36 +1,64 @@
 package s23back.friendList.web;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import s23back.friendList.domain.Friend;
 
 @Controller
 public class FriendListController {
+
+	// Mihin kohtaan koodi, jolla listalle lisätään kaveri?
 	
-	// missä friends-listaa kuuluisi säilyttää?
-	// Miten/missä listalle lisätään kaveri?
-	
-	@GetMapping("/index")
-	public String friendList(@RequestParam(name="friend") String friend, Model model) {
-//		Friend friend1 = new Friend("Mikko Mallikas");
-//		Friend friend2 = new Friend("Maija Poppanen");
-//		ArrayList<Friend> friends = new ArrayList<Friend>();
-//		friends.add(friend1);
-//		friends.add(friend2);
-		model.addAttribute("friend", friend);
+	ArrayList<Friend> friends = new ArrayList<Friend>();
+
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String index(Model model) {
+		model.addAttribute("friends", friends);
 		return "friendlist";
 	}
 	
-	@PostMapping("/index")
-	public String friendSubmit(@ModelAttribute Friend friend, Model model)
-	{
-		model.addAttribute("friend", friend);
-		return "redirect:/friendlist";
+	@RequestMapping(value = "/index", method = RequestMethod.POST)
+	public String addFriendt(@ModelAttribute String name, Model model) {
+		// Friend friend = new Friend(name);
+		// friends.add(friend);
+		model.addAttribute("name", name);
+		return "friendlist";
 	}
 
+	
+// This version works with only 1 friend
+//
+//	@RequestMapping(value = "/index", method = RequestMethod.GET)
+//	public String greetingForm(Model model) {
+//		model.addAttribute("friend", new Friend());
+//		return "friendlist";
+//	}
+//
+//	@RequestMapping(value = "/index", method = RequestMethod.POST)
+//	public String greetingSubmit(@ModelAttribute Friend name, Model model) {
+//		model.addAttribute("friend", name);
+//		return "friendlist";
+//	}
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
